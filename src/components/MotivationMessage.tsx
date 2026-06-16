474b35c969c6a26b183fe4d38407359b19956cdc
97matsu-rgb <97matsu@gmail.com>
2026-06-16 17:49:59 +0900

    Delete src directory

--- src/components/MotivationMessage.tsx
+++
@@ -1,6 +0,0 @@
-export const MotivationMessage = ({ message }: { message: string }) => (
-  <section className="card gradient-card">
-    <h2>モチベーションメッセージ</h2>
-    <p>{message}</p>
-  </section>
-);
